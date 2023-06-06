d0ad794d6bef768c51b79a0017975ab51d10c03b
dominic-source <chinonsodomnic@gmail.com>
2023-06-06 14:10:45 -0700

    print digit combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    for n in range(i, 10):
+        if i < n:
+            if n > 1:
+                print(",", end=' ')
+            print("{}{}".format(i,n), end='')
+print("")
