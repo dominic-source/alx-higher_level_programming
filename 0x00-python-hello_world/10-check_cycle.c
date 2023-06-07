6073188b08ed41440d67914aeceedc80c96e3e6c
dominic-source <chinonsodomnic@gmail.com>
2023-06-07 10:49:24 -0700

    make linked list check more efficient or optimized v1.3

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -22,38 +22,3 @@ int check_cycle(listint_t *list)
 	}
 	return (0);
 }
-
-/*
-int c2heck2cycle2(listint_t *list)
-{
-	long *items;
-	size_t i;
-	int j;
-
-	items = malloc(sizeof(long int) * 2);
-	if (items == NULL)
-		return (-1);
-	for (i = 0; list != NULL; i++)
-	{
-		if (i != 0)
-		{
-			items = realloc(items, sizeof(long int) * (i + 2));
-			if (items == NULL)
-				return (-1);
-		}
-		items[i] = (long int)list;
-		items[i + 1] = '\0';
-		list = list->next;
-		for (j = 0; items[j] != '\0'; j++)
-		{
-			if (items[j] == (long int)list)
-			{
-				free(items);
-				return (1);
-			}
-		}
-	}
-	free(items);
-	return (0);
-}
-*/
