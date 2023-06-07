86628788b599e54e376ca69b0244586c022cc6bb
dominic-source <chinonsodomnic@gmail.com>
2023-06-07 10:48:08 -0700

    make linked list check more efficient or optimized v1.2

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,6 +10,8 @@ int check_cycle(listint_t *list)
 	listint_t *loop1 = list;
 	listint_t *loop2 = list;
 
+	if (list == NULL)
+		return (0);
 	while (loop1 != NULL && loop2 != NULL)
 	{
 		loop1 = loop1->next;
