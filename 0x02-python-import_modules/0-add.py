0b17b851c0d26d1d7e9f2cefff8385290be66b57
dominic-source <chinonsodomnic@gmail.com>
2023-06-08 12:14:07 -0700

    replace wrong add file with 0-add file

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from add_0 import add
+
+def print_add():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == '__main__':
+    print_add()
