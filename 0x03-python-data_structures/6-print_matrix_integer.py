b6a8ad94eaa9cdec23dbf2b5bb892e87590c00ae
dominic-source <chinonsodomnic@gmail.com>
2023-06-09 15:08:08 -0700

    print matrix with the print_matrix_integer file

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for d, n in enumerate(i):
+            if d != 0:
+                print(' ', end='')
+            print('{}'.format(n), end='')
+        print('')
+
+
+if __name__ == '__main__':
+    print_matrix_integer()
