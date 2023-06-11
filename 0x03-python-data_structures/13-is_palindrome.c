74532eac4c5fc3b6db13f48ce2761489486bfc47
dominic-source <chinonsodomnic@gmail.com>
2023-06-11 12:10:08 -0700

    Make finding a palindrome more efficient v1.10

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -13,17 +13,17 @@ int is_palindrome(listint_t **head)
 	for (i = 0; compare != NULL; i++)
 	{
 		count_c++;
-		sum += compare->n;
+		sum += (compare->n * compare->n);
 		compare = compare->next;
 	}
 	count = count_c / 2;
 	for (i = 0; i < count; i++)
 	{
-		sum2 += current->n;
+		sum2 += (current->n * current->n);
 		current = current->next;
 	}
 	if ((count_c % 2) != 0)
-		sum2 += (current->n) / 2;
+		sum2 += (current->n * current->n) / 2;
 	if ((sum / 2) != sum2)
 		return (0);
 	return (1);
