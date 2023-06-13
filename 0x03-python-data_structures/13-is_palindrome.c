83b84660030f26639b08084f9d69e87ba4e84c5e
dominic-source <chinonsodomnic@gmail.com>
2023-06-13 05:31:36 -0700

    improve the performance of finding a palindrome with Task 13 v1.3

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -14,17 +14,24 @@ int is_palindrome(listint_t **head)
 	for (i = 0; compare != NULL; i++)
 	{
 		count_c++;
-		sum += compare->next != NULL ? (compare->n * compare->next->n): 0;
+		sum += compare->next != NULL ?
+			((compare->n == 0 ?
+			  1 : compare->n) * (compare->next->n == 0 ?
+				       1 : compare->next->n)) : 0;
 		compare = compare->next;
 	}
 	count = count_c / 2;
 	for (i = 0; i < count; i++)
 	{
-		sum2 += (current->n * current->next->n);
+		sum2 += ((current->n == 0 ?
+			  1 : current->n) * (current->next->n == 0 ?
+					     1 : current->next->n));
 		current = current->next;
 	}
 	if ((count_c % 2) == 0 && count_c != 0)
-		sum2 -= ((float)(current->n * current->n)) / 2;
+		sum2 -= (float)((current->n == 0 ?
+				 1 : current->n) * (current->n == 0 ?
+						    1 : current->n)) / 2;
 	if ((sum / 2) != sum2)
 		return (0);
 	return (1);
