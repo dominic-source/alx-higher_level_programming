1fabb62b7f60219095dd878ac3eef6dca4192e6d
dominic-source <chinonsodomnic@gmail.com>
2023-06-14 11:40:08 -0700

    find and replace numbers

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return new_list
+
+
+if __name__ == '__main__':
+    search_replace([], 0, 0)
