a0865a150d1656e5080a22335c21a2dfc8535de5
dominic-source <chinonsodomnic@gmail.com>
2023-06-16 11:45:37 -0700

    create file and the function prototype

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,30 @@
+#include <Python.h>
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+
+/**
+ * print_python_list - print python list
+ * @p: python list
+ */
+void print_python_list(PyObject *p)
+{
+
+
+
+
+}
+
+
+
+
+/**
+ * print_python_bytes - print python bytes
+ * @p: python object
+ */
+void print_python_bytes(PyObject *p)
+{
+
+
+
+}
