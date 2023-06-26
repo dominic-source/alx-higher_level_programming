e8c5172e630144b28b08f7185a9479d27568d043
dominic-source <chinonsodomnic@gmail.com>
2023-06-26 15:09:48 -0700

    safely make function call

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except Exception as ex:
+        print("Exception:", ex, file=sys.stderr)
+        return None
+    return result
