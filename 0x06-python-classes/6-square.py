7e6b00c2f23877899c03884f4dfb09ce87d0a30f
dominic-source <chinonsodomnic@gmail.com>
2023-06-28 15:41:07 -0700

    debug - square class v2.7

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -125,9 +125,10 @@ class Square:
         if (self.__size == 0):
             print("")
         else:
+            for y in range(self.__position[1]):
+                print("")
             for i in range(self.__size):
                 for m in range(self.__position[0]):
-                    if self.__position[1] <= 0:
                         print(" ", end='')
                 for j in range(self.__size):
                     print('#', end='')
