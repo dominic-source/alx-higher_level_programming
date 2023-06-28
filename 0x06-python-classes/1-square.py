7a2eabc392b3793e3d594ceaf33ce495bfd33860
dominic-source <chinonsodomnic@gmail.com>
2023-06-28 05:48:02 -0700

    create private instance attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+
+""" square docstings.
+
+   This module tries to define a square from the previous square
+   module (0-square).
+
+   Example:
+      $ ./1-square.py
+"""
+
+class Square:
+    """create a private attribute
+
+    Arg:
+        size(int): this is the size of the square
+
+    Attributes:
+        size(int): this is a private attribute
+
+    """
+    def __init__(self, size):
+        """ the __init__ method
+
+        Args:
+            size: a private instance attribute
+
+        """
+        self.__size = size
