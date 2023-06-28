9b0aa3197c8fbfd8f01bfb5185d812263f717ce6
dominic-source <chinonsodomnic@gmail.com>
2023-06-28 12:51:11 -0700

    debug - further understanding of classes v1.5

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -96,8 +96,8 @@ class Square:
 
         for i in value:
             if i < 0:
-                raise TypeError("position must be a tuple of 2
-                 positive integers")
+                raise TypeError("position must be a tuple of 2"
+                 " positive integers")
         self.__position = value
 
     def area(self):
