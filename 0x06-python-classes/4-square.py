a7762649e412e0926fada8dc1e8c5b530d44ccf5
dominic-source <chinonsodomnic@gmail.com>
2023-06-28 08:04:59 -0700

    further your undertanding about classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+"""module that helps us understand the private instance attribute
+
+It has the a square class with a private instance attribute
+
+Example:
+    $./4-square.py
+
+"""
+
+
+class Square:
+
+    """defines what the class
+
+    This includes the __init__ method and a private instance attribute
+
+    Args:
+       size (int): the values for the argument
+
+    Attributes:
+       size (int): the private instance attribute
+
+    """
+
+    def __init__(self, size=0):
+
+        """Object initialization
+
+        This will initialize the class object once instantiated
+
+        Args:
+            size (int): the size of the square
+
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+
+        """retrieve the data
+
+        This method will retrieve data from the class
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        """Set the size of the object
+
+        This method will set the size of the square
+
+        Args:
+            value (int): the value of size
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+
+        """Calculate the area
+
+        This method will calculate the area
+
+        """
+
+        return self.__size * self.__size
