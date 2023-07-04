7d6ee0f5c8ce9632e097a7ac4935ae2478d38a80
dominic-source <chinonsodomnic@gmail.com>
2023-07-04 06:55:04 -0700

    debug class lock

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -9,18 +9,18 @@ class LockedClass:
 
     """A locked class"""
 
-    cn = 'LockedClass'
-
     def __setattr__(self, nm, value):
 
         """Set Attribute method which is always called"""
 
-        if nm.lower() != "first_name":
-            raise AttributeError(f"'{self.cn}' object has no attribute '{nm}'")
+        cn = 'LockedClass'
+        if nm != "first_name":
+            raise AttributeError(f"'{cn}' object has no attribute '{nm}'")
 
     def __delattr__(self, nm):
 
         """delete attribute method which is also when deleting attribute"""
 
-        if nm.lower() != "first_name":
-            raise AttributeError(f"'{self.cn}' object has no attribute '{nm}'")
+        cn = 'LockedClass'
+        if nm != "first_name":
+            raise AttributeError(f"'{cn}' object has no attribute '{nm}'")
