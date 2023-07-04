cd71000050782e64156602beb63fd8ae70ce845c
dominic-source <chinonsodomnic@gmail.com>
2023-07-04 10:01:54 -0700

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -9,6 +9,8 @@ class LockedClass:
 
     """A locked class"""
 
+    __allowed_attribute = {"first_name"}
+
     def __init__(self):
 
         """init function"""
@@ -19,8 +21,8 @@ class LockedClass:
 
         """Set Attribute method which is always called"""
 
-        if nm == "first_name":
-            object.__setattr__(self, nm, value)
-        else:
+        if nm not in self.__allowed_attribute:
             cl = self.__class__.__name__
             raise AttributeError(f"'{cl}' object has no attribute '{nm}'")
+        else:
+            object.__setattr__(self, nm, value)
