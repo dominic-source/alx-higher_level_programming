83b07ef96c6dbf40ad9c2aea91eb83fb8e44da90
dominic-source <chinonsodomnic@gmail.com>
2023-07-04 06:30:53 -0700

    create a locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""This module will lock the class
+
+"""
+
+
 class LockedClass:
 
     """A locked class"""
@@ -7,9 +12,14 @@ class LockedClass:
     cn = 'LockedClass'
 
     def __setattr__(self, name, value):
+
+        """Set Attribute method which is always called"""
+
         if name != "first_name":
             raise AttributeError(f"'{self.cn}' object has no attribute {name}")
 
     def __delattr__(self, name):
+
+        """delete attribute method which is also when deleting attribute"""
         if name != "first_name":
             raise AttributeError(f"'{self.cn}' object has no attribute {name}")
