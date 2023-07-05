ce324a4846658a191dfdf7289067bbd39d5475e5
dominic-source <chinonsodomnic@gmail.com>
2023-07-05 19:26:29 +0100

    create singly_linked_list with python

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -15,7 +15,7 @@ class Node:
 
     """
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
 
         """The initialization function
 
@@ -26,7 +26,7 @@ class Node:
         """
 
         self.data = data
-        self.new_node = next_node
+        self.next_node = next_node
 
     @property
     def data(self):
@@ -39,11 +39,76 @@ class Node:
 
         return self.__data
 
-    @data.property
-    def data(self):
+    @data.setter
+    def data(self, value):
         """set the data
 
         This will help set the data
 
         """
-        self.__data = data
+
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+
+        self.__data = value
+
+    @property
+    def next_node(self):
+
+        """Get the next node"""
+
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+
+        """Set the next node"""
+
+        if value is None:
+            pass
+        elif not (isinstance(value, Node)):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList():
+
+    """A singly linked class
+
+    """
+
+    def __init__(self):
+        """initialization"""
+        self.__head = None
+
+    def sorted_insert(self, value):
+
+        """Insert the linked list in a sorted order"""
+
+        if (self.__head is None) or value < self.__head.data:
+            self.__head = Node(value, self.__head)
+
+        else:
+            crt = self.__head
+            while(crt.next_node is not None and crt.next_node.data <= value):
+                crt = crt.next_node
+
+            if crt.next_node is None:
+                newNode = Node(value)
+                crt.next_node = newNode
+            else:
+                newNode = Node(value, crt.next_node)
+                crt.next_node = newNode
+
+    def __str__(self):
+
+        """String representation of the class"""
+
+        current = self.__head
+        mstr = ""
+        while (current is not None):
+            mstr += str(current.data)
+            mstr += "\n"
+            current = current.next_node
+
+        return mstr[:-1]
