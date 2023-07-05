a413c69ac557fd5a44801677712ce7305599d4a3
dominic-source <chinonsodomnic@gmail.com>
2023-07-05 14:06:20 +0100

    test changes

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,14 +7,14 @@ The function in the module will print the first and last name
 """
 
 
-def say_my_name(first_name, last_name=""):
+def say_my_name(first_name="", last_name=""):
 
     """Say my first and last name"""
 
-    if not isinstance(first_name, str):
+    if (first_name == "") or not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
-    if not isinstance(last_name, str):
+    if (last_name == "") or not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
