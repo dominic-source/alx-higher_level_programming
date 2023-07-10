7f767ebb1db1764b934fd31585fd06374b256449
dominic-source <chinonsodomnic@gmail.com>
2023-07-10 15:21:58 -0700

    detect object type

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Module check for the instance of a class
+
+"""
+
+def is_same_class(obj, a_class):
+    """an isinstance function
+
+    Checks for an instance of the class similarity
+    """
+
+    if type(obj) is a_class:
+        return True
+    return False
