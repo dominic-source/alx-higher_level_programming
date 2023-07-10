74b20aec14e42504baa592b1a660792f669d3f23
dominic-source <chinonsodomnic@gmail.com>
2023-07-10 15:28:40 -0700

    find the kind of the class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Module that finds kind of class
+
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """the kind of class module
+
+    """
+    return isinstance(obj, a_class)
