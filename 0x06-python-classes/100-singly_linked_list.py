70fadc599b3e3d409aa456d7ad1cbaa595d9209b
dominic-source <chinonsodomnic@gmail.com>
2023-07-10 13:32:43 -0700

    singly_linked_list merge

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -15,7 +15,7 @@ class Node:
 
     """
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
 
         """The initialization function
 
@@ -26,7 +26,7 @@ class Node:
         """
 
         self.data = data
-        self.new_node = next_node
+        self.next_node = next_node
 
     @property
     def data(self):
@@ -40,10 +40,62 @@ class Node:
         return self.__data
 
     @data.property
-    def data(self):
+    def data(self, value):
         """set the data
 
         This will help set the data
 
         """
-        self.__data = data
+
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Retrieve the next node
+
+        This will retrieve the next node
+
+        """
+        return self.__next_node
+
+    @next_node.property
+    def next_node(self, value):
+        """Set the next node
+
+        This will set the next node of the node
+
+        Args:
+            value (Node): the node
+
+        """
+
+        if ((value is not None) or (not isinstance(value, Node))):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Create the head node and add other methods to interact with the list
+
+    """
+
+    def __init__(self):
+        """initialization of the singly linked list"""
+
+        self.__head = Node()
+
+    def __str__(self):
+        """A single linked list"""
+
+        current = self.__head
+        while current is not None:
+            print(current.data, end='')
+            current = current.next_node
+        return ""
+
+    def sorted_insert(self, value):
+        if self.__head.next_node == None:
+            self.__head.data = value
+        elif
--- 0x09-python-everything_is_object/main.py
+++ 0x09-python-everything_is_object/main.py
@@ -3,8 +3,23 @@ LockedClass = __import__('101-locked_class').LockedClass
 
 lc = LockedClass()
 lc.first_name = "John"
+print(lc.first_name)
+print("--")
 
-try:
-    lc.First_Name = "Snow"
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+lc.first_name = "Doe"
+print(lc.first_name)
+print("--")
+
+lc.first_name = "Jane"
+lc.first_name = "Tunde"
+print(lc.first_name)
+print("--")
+
+obj = LockedClass()
+obj.first_name = "John"
+del obj.first_name
+print(hasattr(obj, "first_name"))
+print("--")
+
+obj = LockedClass()
+obj.last_name = "Doe"
