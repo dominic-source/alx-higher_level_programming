f1068380c8d7d820792dc096072bf4655d2bdd54
dominic-source <chinonsodomnic@gmail.com>
2023-07-10 16:38:34 -0700

    raise errors

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+"""Module that create a geometry
+
+"""
+
+
+class BaseGeometry:
+
+    """The base Geometry class
+
+    """
+
+    def area(self):
+        """raise an exception
+
+        a method that raises an exception when called
+
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate integers
+
+        this will validate integers
+
+        """
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
