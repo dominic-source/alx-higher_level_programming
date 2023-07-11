a4f206c24689dd8e985e6064ba7bb41512a85fe7
dominic-source <chinonsodomnic@gmail.com>
2023-07-10 17:52:19 -0700

    task 9 rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -9,6 +9,7 @@ Checks out a rectangle
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """A class to practice inheritance
 
@@ -21,3 +22,19 @@ class Rectangle(BaseGeometry):
         self.integer_validator(height, height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+
+        """Implement the area method
+
+        """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+
+        """string representation of the data"""
+
+        string = "[Rectangle] " + str(self.__width) + '/' + str(self.__height)
+
+        return string
