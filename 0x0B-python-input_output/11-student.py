d26f062b3a45449c36093bb06688504b8481154a
dominic-source <chinonsodomnic@gmail.com>
2023-07-11 23:36:04 +0100

    reload attributes from json file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""Implement a student module
+
+"""
+
+
+class Student:
+    """A class to define student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the class
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """convert data to json"""
+
+        if attrs is None:
+            return self.__dict__
+        elif not isinstance(attrs, list):
+            return self.__dict__
+        elif isinstance(attrs, list):
+            for i in attrs:
+                if not isinstance(i, str):
+                    return self.__dict__
+        new_list = []
+        for i in attrs:
+            if hasattr(self, i):
+                new_list.append((i, self.__dict__[i]))
+        return dict(new_list)
+
+    def reload_from_json(self, json):
+        """reload attributes from json"""
+
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
