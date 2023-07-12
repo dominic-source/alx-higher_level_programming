30a6297a7fa6d04e1f67160beea9817dbc07e49b
dominic-source <chinonsodomnic@gmail.com>
2023-07-12 16:12:08 +0100

    print statistics v3

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,30 +10,30 @@ import sys
 def my_main():
     """The function that will solve the problem"""
 
-    status = {'200': '0', '301': '0', '400': '0', '401': '0',
-              '403': '0', '404': '0', '405': '0', '500': '0'}
+    status = {'200': 0, '301': 0, '400': 0, '401': 0,
+              '403': 0, '404': 0, '405': 0, '500': 0}
     size = 0
     count = 0
     try:
         for line in sys.stdin:
             line = line.strip()
             line_split = line.split()
-            file_size = line_split[-1]
+            file_size = int(line_split[-1])
             status_code = line_split[-2]
 
-            status[status_code] = str(int(status[status_code]) + 1)
-            size += int(file_size)
+            status[status_code] += 1
+            size += file_size
             count += 1
             if count % 10 == 0:
-                print("File size: {:d}".format(int(size)))
+                print("File size: {:d}".format(size))
                 for k, v in sorted(status.items()):
-                    if int(v) > 0:
-                        print("{}: {:d}".format(k, int(v)))
+                    if v > 0:
+                        print("{}: {:d}".format(k, v))
     except KeyboardInterrupt:
-        print("File size: {:d}".format(int(size)))
+        print("File size: {:d}".format(size))
         for k, v in sorted(status.items()):
-            if int(v) > 0:
-                print("{}: {:d}".format(k, int(v)))
+            if v > 0:
+                print("{}: {:d}".format(k, v))
 
 
 my_main()
