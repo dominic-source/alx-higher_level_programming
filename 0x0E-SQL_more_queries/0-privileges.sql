c2c2eede1f541e83812384982f4b6e3f0f5ec0e4
dominic-source <chinonsodomnic@gmail.com>
2023-08-16 14:44:14 -0700

    list grants of users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- list all privileges
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS
+     FOR 'user_0d_1'@'localhost';
+SHOW GRANTS
+     FOR 'user_0d_2'@'localhost';
