95e5b9b65f4790a865fa391170527f9bb901a05a
dominic-source <chinonsodomnic@gmail.com>
2023-08-16 06:24:20 -0700

    modify field

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,12 @@
 --  convert database to utf-8
--- Alter name field
-ALTER TABLE hbtn_0c_0.first_table
-MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- alter table
-ALTER TABLE hbtn_0c_0.first_table
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- alter database
+-- Alter Database
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Alter Table and field
+ALTER TABLE hbtn_0c_0.first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Alter field
+ALTER TABLE hbtn_0c_0.first_table
+MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
