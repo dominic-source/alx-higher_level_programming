21709f8c6b29a659c181c35f4cb1aa5410f3a02b
dominic-source <chinonsodomnic@gmail.com>
2023-08-17 08:43:08 -0700

    create cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
 -- create database and cities table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT NOT EXIST hbtn_0d_usa.cities(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
        id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-       state_id INT NOT NULL FOREIGN KEY(id) REFERENCE states(id),
-       name VARCHAR(256) NOT NULL);
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
