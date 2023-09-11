411d3f4ce45e9c85216c5f3a8aab499197f6e689
dominic-source <chinonsodomnic@gmail.com>
2023-09-11 15:32:57 -0700

    factorial func create

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
 
+function factorial (num) {
+  if (num < 1) return (1);
+  return (num * factorial(num - 1));
+}
+
 const pros = Number(process.argv[2]);
 if (!pros) console.log('1');
 else {
-  factorial(pros);
+  console.log(factorial(pros));
 }
