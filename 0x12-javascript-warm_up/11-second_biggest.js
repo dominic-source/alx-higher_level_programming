1f2d409f63f4f8394848981fb44b16e67d462070
Dominic <68060853+dominic-source@users.noreply.github.com>
2023-09-12 01:30:23 +0100

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,19 @@
+#!/usr/bin/node
 
+const pros = process.argv;
+if (pros.length === 0 || pros.length === 1) {
+  console.log(0);
+}
+else {
+  let i = 0;
+  let j = 0;
+  let k;
+  for (k = 0; k < pros.length; k++) {
+    if (pros[k] > i) {
+      j = i;
+      i = pros[k];
+    } else if (pros[k] > j) {
+      j = pros[k];
+    }
+  }
+}
