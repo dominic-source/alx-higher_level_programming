ac080ca26a313664e96669cb7fb827c1267afdb8
dominic-source <chinonsodomnic@gmail.com>
2023-09-13 09:27:58 +0100

    inherit from square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,40 +1,9 @@
 #!/usr/bin/node
+const square = require('./5-square.js');
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    let i;
-    let j;
-    for (i = 0; i < this.height; i++) {
-      let x = '';
-      for (j = 0; j < this.width; j++) {
-        x += 'X';
-      }
-      console.log(x);
-    }
-  }
-
-  rotate () {
-    const hold = this.width;
-    this.width = this.height;
-    this.height = hold;
-  }
-
-  double () {
-    this.width = 2 * this.width;
-    this.height = 2 * this.height;
-  }
-}
-
-class Square extends Rectangle {
+class Square extends square {
   constructor (size) {
-    super(size, size);
+    super(size);
     this.size = size;
   }
 
