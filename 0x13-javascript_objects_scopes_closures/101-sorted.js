8e6a6ad1c6686ec88da993f67bd50b358cabaae8
dominic-source <chinonsodomnic@gmail.com>
2023-09-12 17:25:20 -0700

    print dict value to key

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 const { dict } = require('./101-data.js');
 
-let i;
-for (i =
+const newdict = {};
+let key;
+for (key in dict) {
+  const nkey = dict[key];
+  if (newdict[nkey]) newdict[nkey].push(key);
+  else newdict[nkey] = [key];
+}
+console.log(newdict);
