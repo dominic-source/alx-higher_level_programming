bb76a7ec86f98ace4bcc40ad21b022e2c7ea4d88
dominic-source <chinonsodomnic@gmail.com>
2023-09-17 10:43:22 -0700

    use sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""This module creates a function that list all
+states with a name as the last argument of the command
+
+It will do it safely
+"""
+
+import sys
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy import select
+
+
+def name_states():
+    """list state that is name"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    port_n = 3306
+
+    url = "mysql+mysqldb://{}:{}@localhost:{}/{}".format(username,
+                                                         password, port_n,
+                                                         database)
+    engine = create_engine(url)
+    Base.metadata.create_all(engine)
+    query = select([State.id, State.name]).order_by(State.id)
+    session = Session(bind=engine)
+    for row in session.connection().execute(query).fetchall():
+        print(str(row[0]) + ": " + str(row[1]))
+
+
+if __name__ == "__main__":
+    name_states()
