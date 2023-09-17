1b3ee597b663f6321e9c891c7acf0541b30a2cc4
dominic-source <chinonsodomnic@gmail.com>
2023-09-17 05:07:56 -0700

    safe filtering

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""This module creates a function that list all
+states with a name as the last argument of the command
+
+It will do it safely
+"""
+import MySQLdb
+import sys
+
+
+def name_states():
+    """list state that is name"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+    port_n = 3306
+
+    db = MySQLdb.connect(host='localhost', port=port_n, passwd=password,
+                         db=database, user=username)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name = %s\
+    ORDER BY states.id ASC", (state_name,))
+    for data in cur.fetchall():
+        print(data)
+    db.commit()
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    name_states()
