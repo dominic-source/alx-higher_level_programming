14a97811faced8812655f12a2945145ee97422f4
dominic-source <chinonsodomnic@gmail.com>
2023-09-18 06:28:52 -0700

    relationship city

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""This module creates a function that will manipulate state and city models
+
+"""
+
+import sys
+from sqlalchemy.orm import Session, relationship
+from sqlalchemy import create_engine
+from relationship_city import City
+from relationship_state import State, Base
+
+
+def name_states():
+    """list state that is name"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    port_n = 3306
+
+    url = "mysql+mysqldb://{}:{}@localhost:{}/{}".format(username,
+                                                         password,
+                                                         port_n,
+                                                         database)
+
+    engine = create_engine(url)
+    Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+    for state in session.query(State).join(State.cities).order_by(
+            State.id).order_by(City.id).all():
+        string = "{}: {}".format(state.id, state.name)
+        print(string)
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
+
+    session.commit()
+
+
+if __name__ == "__main__":
+    name_states()
