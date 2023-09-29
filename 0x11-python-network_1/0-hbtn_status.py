e3e9da59b9f17ad38e7590ad2fce309694058829
dominic-source <chinonsodomnic@gmail.com>
2023-09-29 11:20:56 -0700

    remove blanks and replace it with tab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,9 +12,9 @@ def urlfetch():
     with urlopen(url) as response:
         obj = response.read()
         print('Body response:')
-        print('    - type: {}'.format(type(obj)))
-        print('    - content: {}'.format(obj))
-        print('    - utf8 content: {}'.format(obj.decode('utf-8')))
+        print('\t- type: {}'.format(type(obj)))
+        print('\t- content: {}'.format(obj))
+        print('\t- utf8 content: {}'.format(obj.decode('utf-8')))
 
 
 if __name__ == '__main__':
