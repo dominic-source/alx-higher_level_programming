15752492553b5b47374cc40b96541cc4a4834028
dominic-source <chinonsodomnice@gmai.com>
2023-09-29 15:09:16 -0700

    get github info

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""This module will fetch a response from a server
+
+"""
+
+import requests
+import sys
+
+
+def urlfetch():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    headers = {'Accept': 'application/vnd.github+json',
+               'Authorization': f'Bearer {password}'}
+    url = f'https://api.github.com/users/{username}'
+    res = requests.get(url, headers=headers)
+    print(res.json()['id'])
+
+
+if __name__ == '__main__':
+    urlfetch()
