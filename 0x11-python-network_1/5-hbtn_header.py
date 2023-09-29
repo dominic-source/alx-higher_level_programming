d41a13d5f25a75a803b7f47ee8fba2a52f4ebfb0
dominic-source <chinonsodomnice@gmai.com>
2023-09-29 12:32:27 -0700

    print response headers using Requests

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""This module will fetch a response from a server
+
+"""
+
+import requests
+import sys
+
+
+def urlfetch():
+    url = sys.argv[1]
+    res = requests.get(url)
+    print(res.headers['X-Request-Id'])
+
+
+if __name__ == '__main__':
+    urlfetch()
