6e0297eecb5052c2ff795e2a835551ac6e0f5e81
dominic-source <chinonsodomnic@gmail.com>
2023-10-02 16:43:21 -0700

    improve time complexity with help

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,6 +11,28 @@ def find_peak(list_of_integers):
         return None
     elif len(mlist) < 2:
         return mlist[0]
+    else:
+        left = 0
+        right = len(mlist) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if mlist[mid] < mlist[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+        return mlist[left]
+
+
+def find_peak22(list_of_integers):
+    """This function will find a peak"""
+
+    mlist = list_of_integers
+    if not mlist:
+        return None
+    elif len(mlist) < 2:
+        return mlist[0]
     elif len(mlist) == 2:
         if mlist[0] < mlist[1]:
             return mlist[1]
