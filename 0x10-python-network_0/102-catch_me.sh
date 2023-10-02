7734c4d74582b9cf6440840d38fedb3d7ba5cd2f
dominic-source <chinonsodomnic@gmail.com>
2023-10-02 01:14:58 -0700

    debug

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # make the server say "you got me"
-curl -sLX PUT "0.0.0.0:5000/catch_me" -d "user_id=98" -H "origin: School"
+curl -sLX PUT "0.0.0.0:5000/catch_me_2" -d "user_id=98" -H "origin: School"
